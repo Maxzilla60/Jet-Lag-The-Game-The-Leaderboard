05b4fd926ab48c71955e6761cd7759fe2cf9d172
Maxzilla60 <maxpower98@hotmail.com>
2024-10-04 22:23:52 +0200

    ♻️ Improve use of wikitable2json API

--- src/via-api.ts
+++ src/via-api.ts
@@ -1,4 +1,4 @@
-import { flattenDeep } from 'lodash';
+import { values } from 'lodash';
 
 export interface ApiJetLagSeason {
   seasonNumber: number;
@@ -6,79 +6,32 @@ export interface ApiJetLagSeason {
   winners: string[];
 }
 
-export async function fetchSeasonsData(): Promise<ApiJetLagSeason[]> {
-  const allTablesFromWikiPage: string[][][] = await fetch('https://www.wikitable2json.com/api/Jet_Lag:_The_Game')
-    .then(response => response.json());
+interface TableRow {
+  Season: string;
+  Title: string;
+  'Winner(s)': string;
+}
 
-  const seasonsTable: string[][] | undefined = findSeasonsTable(allTablesFromWikiPage);
+export async function fetchSeasonsData(): Promise<ApiJetLagSeason[]> {
+  const seasonsTable: TableRow[] = await fetch('https://www.wikitable2json.com/api/Jet_Lag:_The_Game?table=1&keyRows=2')
+    .then(response => response.json())
+    .then(json => {
+      if (!json || json.length === 0) {
+        return undefined;
+      }
+      return json[0];
+    });
 
   if (!seasonsTable) {
     throw new Error('Couldn\'t find seasons table');
   }
 
-  const headers = findHeaders(seasonsTable);
-
   return seasonsTable!
-    .filter(row => !flattenDeep(row).some(cell => cell.includes('Title'))) // Filter out headers
-    .filter(row => !flattenDeep(row).some(cell => cell.includes('TBA'))) // Filter out unfinished seasons
-    .map(row => mapRow(row, headers));
-}
-
-// ---
-
-function findSeasonsTable(tables: string[][][]) {
-  return tables.find(table => {
-    const flattened = flattenDeep(table);
-    return flattened.some(cell => cell.includes('Winner'));
-  });
-}
-
-function findHeaders(seasonsTable: string[][]): { headerCell: string, index: number }[] {
-  const headerRow = seasonsTable[0];
-
-  if (!headerRow) {
-    throw new Error('Couldn\'t find header row');
-  }
-
-  const headers = headerRow
-    .map((headerCell, index) => ({
-      headerCell,
-      index,
+    .filter(row => !values(row).some(cell => cell.includes('TBA'))) // Filter out unfinished seasons
+    .map(row => ({
+      seasonNumber: Number(row.Season),
+      seasonName: row.Title,
+      winners: row['Winner(s)'].split(' & '),
     }))
-    .filter(({ headerCell }) => {
-      return headerCell.includes('Season')
-        || headerCell.includes('Title')
-        || headerCell.includes('Winner');
-    });
-
-  if (headers.length !== 3) {
-    throw new Error('Couldn\'t retrieve "Season", "Title", and "Winner" header cells');
-  }
-
-  return headers;
-}
-
-function mapRow(row: string[], headers: { headerCell: string; index: number }[]): ApiJetLagSeason {
-  return {
-    seasonNumber: Number(getColumnIndexForHeader(row, headers, 'Season')),
-    seasonName: getColumnIndexForHeader(row, headers, 'Title'),
-    winners: getColumnIndexForHeader(row, headers, 'Winner').split(' & '),
-  };
-}
-
-function getColumnIndexForHeader(
-  row: string[],
-  headers: {
-    headerCell: string;
-    index: number
-  }[],
-  headerTitle: string,
-): string {
-  const header = headers.find(({ headerCell }) => headerCell.includes(headerTitle))!;
-
-  if (row.length <= header.index) {
-    throw new Error('Couldn\'t find cell for given header index');
-  }
-
-  return row[header.index];
+    .sort((a, b) => a.seasonNumber - b.seasonNumber);
 }
